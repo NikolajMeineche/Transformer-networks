e1ffdb8ba4f9cd012d31158fc6fe3c9db290addc
johannefranck <jcafranck@gmail.com>
2022-06-06 11:05:08 +0200

    test herfra

--- Decision-transformer original kode/gym/data/returnconvertion.py
+++ Decision-transformer original kode/gym/data/returnconvertion.py
@@ -6,7 +6,7 @@ import d4rl
 
 #Function to convert a return in a specific environment into a normalized return to compare with Chen et al.
 def ConvertReturn(reward, env, dataset):
-    name = f'{env}-{dataset}-v2'
+    name = f'{env}-{dataset}-v2' #ændring
     Env = gym.make(name)
     returns = Env.get_normalized_score(reward)
     return returns
