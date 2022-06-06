f26d3d070af3bfb69ed0fa073672597d3b2f75aa
johannefranck <jcafranck@gmail.com>
2022-06-06 11:44:22 +0200

    Split reward function now functional

--- Decision-transformer original kode/gym/data/splitreward.py
+++ Decision-transformer original kode/gym/data/splitreward.py
@@ -4,32 +4,27 @@ import numpy as np
 import pandas as pd
 import pickle
 
-def splitreward(datapickle):
-    #try:
-    data = pd.read_pickle(r'datapickle')
-    #except:
-    #    print('Ikke en pickle')
-
-    returntogo1 = []
-    returntogo2 = []
-
+def splitreward(env, datatype):
+    name = f'{env}-{datatype}-v2.pkl'
+    data = pd.read_pickle(f'{name}')
     konstant = -1/10
+    for traj in range(len(data)):
+        r_forward = []
+        r_control = []
 
-    for i in range(pd.data.size):
-        for t in pd.data['observations'][0].size:
-            r2 = konstant * np.square(data['actions'][t])
-            r1 = data['rewards'] - r2 # a og b er 1 i halfcheetah
+        for timestep in range(len(data[traj]['rewards'])):
+            r2 = konstant * np.square(np.linalg.norm(data[traj]['actions'][timestep]))
+            r1 = data[traj]['rewards'][timestep] - r2 # a og b er 1 i halfcheetah
+            r_forward.append(r1)
+            r_control.append(r2)
 
-            returntogo1[i].append(r1)
-            returntogo2[i].append(r2)
+        data[traj]['r1'] = np.array([r_forward]).squeeze()
+        data[traj]['r2'] = np.array([r_control]).squeeze()
 
-        data[i].append(returntogo1)
-        data[i].append(returntogo2)
+    n = 'm_reward'
+    with open(f'{n}-{name}', 'wb') as f:
+        pickle.dump(data, f)
 
-    return data
-
-
-
-
+#splitreward('hopper','medium')
 
 
